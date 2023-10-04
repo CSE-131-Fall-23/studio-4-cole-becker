2abf94ef2150d686477578da0fd38a16bd45f6ab
14178 <14178@DESKTOP-TKAGM6F.dhcp.wustl.edu>
2023-10-04 14:08:46 -0500

    Studio4 - Jeffers Becker Zeng

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,30 @@
 package studio4;
 
-import java.awt.Color;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
+import javax.swing.SwingUtilities;
 
 import edu.princeton.cs.introcs.StdDraw;
 
+/**
+ * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
+ */
 public class Flag {
-	public static void main(String[] args) {
-		
-	}
-}
+
+	public static void main(String[] args) throws FileNotFoundException {
+	
+		StdDraw.setPenColor(255, 0, 0);
+
+		StdDraw.filledRectangle(0.15, 0.5, (.33/2), 0.3);
+
+		StdDraw.setPenColor(0, 0, 255);
+
+		StdDraw.filledRectangle(0.85, 0.5, (.33/2), .3);
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.text(.5, .5, "I hate France");
+		StdDraw.line(0, .8, 1, .2);
+		StdDraw.line(0, .2, 1, .8);
+	}}
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,29 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+		int redComp = in.nextInt();
+		int greenComp = in.nextInt();
+		int blueComp = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double x = in.nextDouble();
+System.out.println(x);
+		double y = in.nextDouble();
+		System.out.println(y);
+		double halfWidth = in.nextDouble();
+		System.out.println(halfWidth);	
+		double halfHeight = in.nextDouble();
+				System.out.println(halfHeight);
+
+		
+		StdDraw.setPenColor(redComp, greenComp, blueComp);
+	if(isFilled == true)
+	{
+		StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+	}
+	else
+	{
+		StdDraw.rectangle(x, y, halfWidth, halfHeight);	
+	}
 	}
 }
